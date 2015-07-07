b181060340a13d067d0f2ddf2f9ea97896043d1d
Watershed2106 <you@example.com>
2015-07-07 00:01:47 -0700

    Initial commit

---
+++ app/src/main/java/fu/hao/uidroid_accessibility/MyAccessibility.java
@@ -0,0 +1,95 @@
+package fu.hao.uidroid_accessibility;
+
+/**
+ * Created by watershed2106 on 7/6/15.
+ */
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+@SuppressLint("NewApi")
+public class MyAccessibility extends AccessibilityService {
+    private static final String TAG = "MyAccessibility";
+    String[] PACKAGES = { "com.android.settings" };
+
+    @Override
+    protected void onServiceConnected() {
+        Log.i(TAG, "config success!");
+        AccessibilityServiceInfo accessibilityServiceInfo = new AccessibilityServiceInfo();
+        // accessibilityServiceInfo.packageNames = PACKAGES;
+        accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        accessibilityServiceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+        accessibilityServiceInfo.notificationTimeout = 1000;
+        setServiceInfo(accessibilityServiceInfo);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // TODO Auto-generated method stub
+        int eventType = event.getEventType();
+        String eventText = "";
+        Log.i(TAG, "==============Start====================");
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                eventText = "TYPE_VIEW_CLICKED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                eventText = "TYPE_VIEW_FOCUSED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                eventText = "TYPE_VIEW_LONG_CLICKED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+                eventText = "TYPE_VIEW_SELECTED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                eventText = "TYPE_VIEW_TEXT_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                eventText = "TYPE_WINDOW_STATE_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                eventText = "TYPE_NOTIFICATION_STATE_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+                break;
+            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
+                eventText = "TYPE_ANNOUNCEMENT";
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+                eventText = "TYPE_VIEW_HOVER_ENTER";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+                eventText = "TYPE_VIEW_HOVER_EXIT";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                eventText = "TYPE_VIEW_SCROLLED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+                eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                eventText = "TYPE_WINDOW_CONTENT_CHANGED";
+                break;
+        }
+        eventText = eventText + ":" + eventType;
+        Log.i(TAG, eventText);
+        Log.i(TAG, "=============END=====================");
+    }
+
+    @Override
+    public void onInterrupt() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
